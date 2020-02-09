ac7cb989a1e935492a13ce8bc69eca53552d6d84
Shane Korin <sk@distriqt.com>
2020-02-10 09:58:51 +1100

    remove redux-thunk, not using it

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,14 @@
 /*
   Redux store base, creates and configures the redux store 
   with reducers.
-
-  Using redux-thunk only for the async-ness to add delays,
-  there is no actual API calls used in the app.
 */
 
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import rootReducer from './rootReducer';
 
 const configureStore = () => {
   return createStore(
-    rootReducer,
-    applyMiddleware(thunk)
+    rootReducer
   );
 };
 
